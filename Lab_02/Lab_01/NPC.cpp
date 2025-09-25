09183b15f6df2d005dfaeca4f48126dba36cd208
JadFuhr <C00290965@itcarlow.ie>
2025-09-25 16:41:28 +0100

    removed broken vision cone

--- Lab_02/Lab_01/NPC.cpp
+++ Lab_02/Lab_01/NPC.cpp
@@ -21,20 +21,13 @@ void NPC::npcUpdate(sf::Time dt, const Player& player)
 	npc_sprite.setPosition(pos);
 	npc_sprite.rotate(sf::degrees(5));
 
-	// Update vision cone
-	updateVisionCone();
 
-	// Check player detection
-	if (playerInVision(player))
-		visionCone.setFillColor(sf::Color(255, 0, 0, 100)); // red
-	else
-		visionCone.setFillColor(sf::Color(0, 255, 0, 100)); // green
 }
 
 
 void NPC::npcRender(sf::RenderWindow& window)
 {
-	window.draw(visionCone);
+
 	window.draw(npc);
 	window.draw(npc_sprite);
 
@@ -72,13 +65,7 @@ void NPC::setupNPCSprite()
 	npc_sprite.setOrigin(sf::Vector2f(npc_texture.getSize().x / 2, npc_texture.getSize().y / 2));
 	npc_sprite.setPosition(sf::Vector2f(npc.getPosition().x, npc.getPosition().y));
 
-	// Setup vision cone (triangle shape)
-	visionCone.setPointCount(3);
-	visionCone.setPoint(0, sf::Vector2f(0.f, 0.f));                  // NPC origin
-	visionCone.setPoint(1, sf::Vector2f(coneLength, -coneLength / 2)); // right edge
-	visionCone.setPoint(2, sf::Vector2f(coneLength, coneLength / 2));  // left edge
-	visionCone.setFillColor(sf::Color(0, 255, 0, 100)); // semi-transparent green
-	visionCone.setOrigin(sf::Vector2f(0.f, 0.f));
+
 }
 
 
