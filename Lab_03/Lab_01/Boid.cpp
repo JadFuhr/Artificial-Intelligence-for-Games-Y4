c890fcac7e86cc6deda5984137e30730e5b884f7
JadFuhr <C00290965@itcarlow.ie>
2025-10-01 23:31:33 +0100

    boid constructor complete

--- Lab_03/Lab_01/Boid.cpp
+++ Lab_03/Lab_01/Boid.cpp
@@ -1,9 +1,16 @@
 #include "Boid.h"
+#include <cmath>
 
 Boid::Boid(float radius)
 {
 
+	shape.setRadius(radius);
+	shape.setOrigin(sf::Vector2f(radius, radius));
+	shape.setFillColor(sf::Color::White);
 
+	position = sf::Vector2f(rand() % 800, rand() % 600);	// rand pos
+	velocity = sf::Vector2f((rand() % 100 - 50) / 10.f, (rand() % 100 - 50) / 10.f);		// rand initial velocity in direction
+	acceleration = sf::Vector2f(0.f, 0.f);		// initial accel = 0, accel update each frame later 
 
 }
 
